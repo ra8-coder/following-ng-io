82c95c62170961c2a14b58ef15c36ead80811b1b
ra8-coder <rmvely@gmail.com>
2019-10-18 00:46:19 +0900

    angular.io pt, done with managing data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule  } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {
+	MatInputModule,
+	MatCardModule,
+	MatButtonModule,
+	MatToolbarModule,
+	MatExpansionModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,25 +17,39 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertComponent } from './product-alert/product-alert.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertComponent,
-    ProductDetailsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-    ])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		TopBarComponent,
+		ProductListComponent,
+		ProductAlertComponent,
+		ProductDetailsComponent,
+		CartComponent,
+		ShippingComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		ReactiveFormsModule,
+		HttpClientModule,
+		MatInputModule,
+		MatCardModule,
+		MatButtonModule,
+		MatToolbarModule,
+		MatExpansionModule,
+		RouterModule.forRoot([
+			{ path: '', component: ProductListComponent },
+			{ path: 'products/:productId', component: ProductDetailsComponent },
+			{ path: 'cart', component: CartComponent },
+			{ path: 'shipping', component: ShippingComponent },
+		]),
+		BrowserAnimationsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,23 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+	selector: 'app-product-details',
+	templateUrl: './product-details.component.html',
+	styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
-  product;
+	product
 
-  constructor( private route: ActivatedRoute ) {
+	addToCart(product) {
+		window.alert('Your product has been added to the cart!');
+		this.cartService.addToCart(product);
+	}
 
-  }
+	constructor(
+		private route: ActivatedRoute,
+		private cartService: CartService
+	) { }
 
-  ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      this.product = products[+params.get('productId')];
-    });
-  }
+	ngOnInit() {
+		this.route.paramMap.subscribe(params => {
+			this.product = products[+params.get('productId')];
+		});
+	}
 
 }
